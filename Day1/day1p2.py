2c1b564817168bce4a0cdbd02b71b80434797107
Nate Bruneau <nathbr@microsoft.com>
2020-12-01 17:26:06 -0800

    uploading day 1 problems

--- Day1/day1p2.py
+++ Day1/day1p2.py
@@ -6,12 +6,13 @@ with open('day1.txt','r') as file:
 found = False
 for num1 in data:
     for num2 in data:
-        val = int(num1) + int(num2)
-        if (val == 2020):
-            print("found one at " + num1 + " and " + num2 )
-            mult1 = int(num1)  * int(num2)
-            print("Multiplier is " + str(mult1))
-            found = True
+        for num3 in data:
+            val = int(num1) + int(num2) + int(num3)
+            if (val == 2020):
+                print("found one at " + num1 + " and " + num2 + " and " + num3 )
+                mult1 = int(num1)  * int(num2) * int(num3)
+                print("Multiplier is " + str(mult1))
+                found = True
+                break
+        if found:
             break
-    if found:
-        break
